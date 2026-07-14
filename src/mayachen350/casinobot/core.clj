d97dbe81982f6f928819badf7f39dcb833b525c4
Maya <maychennn@protonmail.com>
2026-07-14 07:52:33 -0400

    feat: working bot skeleton

--- src/mayachen350/casinobot/core.clj
+++ src/mayachen350/casinobot/core.clj
@@ -1,12 +1,23 @@
 (ns mayachen350.casinobot.core
+  (:import [net.dv8tion.jda.api JDABuilder]
+           [java.util EnumSet]
+           [net.dv8tion.jda.api.requests GatewayIntent]
+           [io.github.cdimascio.dotenv Dotenv])
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(def intents 
+  (EnumSet/of
+   GatewayIntent/GUILD_MESSAGES
+    GatewayIntent/GUILD_MESSAGE_REACTIONS
+    GatewayIntent/MESSAGE_CONTENT))
+
+(def token
+  (-> (Dotenv/load)
+      (.get "DISCORD_TOKEN")))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (greet {:name (first args)}))
+  (->
+   (JDABuilder/createLight token intents)
+   (.build)))
